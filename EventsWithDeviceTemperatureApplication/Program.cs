24d32a3aca89329b41f6b3a3affbbf0e631dad55
= <=>
2024-03-09 19:56:23 +0530

    Initial commit

---
+++ EventsWithDeviceTemperatureApplication/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace EventsWithDeviceTemperatureApplication;
+
+public class Program{
+    public static void Main(string[] args){
+        Console.WriteLine("Press any key to run the device.");
+        Console.ReadKey();
+    }
+}
