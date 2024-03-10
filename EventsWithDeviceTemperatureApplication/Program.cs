019cab9ba9d65778b6e8fa5d81a56a952aa0a30d
= <=>
2024-03-10 12:55:07 +0530

    Comments for required reference types like classes and interfaces

--- EventsWithDeviceTemperatureApplication/Program.cs
+++ EventsWithDeviceTemperatureApplication/Program.cs
@@ -1,5 +1,13 @@
 ﻿namespace EventsWithDeviceTemperatureApplication;
 
+//Device class and interface
+
+//CoolingMechanism class and interface
+
+//ThermoStat class and interface
+
+//HeatingSensor class and interface (with relevant event declarations with event list
+
 public class Program{
     public static void Main(string[] args){
         Console.WriteLine("Press any key to run the device.");
